3353b1dfe65b6c17d00bc88185f9f7d27919a418
Karthick M <karthick.mani@syncfusion.com>
2026-06-28 09:13:28 +0530

    Removed test scripts file

--- Gantt/tests/Gantt_Combination/Helper/Helper.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Helper Class
- *
- */
-
-export namespace Helper {
-  export const baseUrl: string = 'http://localhost:5050/gantt-chart/';
-  export const baseUrlserver: string = 'http://localhost:5158';
-};
